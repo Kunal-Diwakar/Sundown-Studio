ec3bcb50cf1eef21c607864fb24f0d87606c80c7
Kunal Diwakar <kunaldiwakar1999@gmail.com>
2024-06-30 17:30:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function loaderAnimation() {
     var loader = document.querySelector("#loader")
     setTimeout(function () {
         loader.style.top = "-100%"
-    }, 4200)
+    }, 4500)
 }
 
 loaderAnimation()
